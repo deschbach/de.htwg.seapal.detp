ac73aac85001c6d6316e1947645efa66fedac072
Dominic Eschbach <doeschba@htwg-konstanz.de>
2012-11-29 12:17:55 +0100

    Modified app_map.php for websocket task.

--- site/app_map.php
+++ site/app_map.php
@@ -8,6 +8,17 @@
     	include('app_map_header.php');
         include('app_menu.php');
     ?>
+    
+    <script Type="text/javascript" src="fancywebsocket.js"></script>
+	<script type="text/javascript" src="chat.js"></script>
+	
+	<link rel="stylesheet" href="style.css" type="text/css" />
+	<style>
+		input, textarea {border:1px solid #CCC;margin:0px;padding:0px}
+		#log {width:100%;height:400px}
+		#message {width:100%;line-height:20px}
+	</style>
+    
 </head>
 <body onload="initialize()">
 
@@ -30,12 +41,20 @@
         </div>
     </div>
     <div id="appWrapper">
-    	<div id="map_canvas"></div>
+    	<div id="map_canvas" style="width:50%; float:left;"></div>
+    	<div id="chat" style="width:50%; background: #EEEEEE; float:right; height:100%;" align="center">
+    		<br>
+    		<div id="chat-area" style="height:60%; width:90%; background-color: white;"></div>
+    		<br>
+    		<form id="send-message-form">
+            	<p>Your message: </p>
+            	<textarea id="send-message-box" maxlength = '100' style="border:1px solid #CCC;margin:0px;padding:0px; width:300px;"></textarea>
+            </form>
+    	</div>
     </div>
     <div id="temporaryMarkerContextMenu"></div>
     <div id="fixedMarkerContextMenu"></div>
     <div id="routeContextMenu_active"></div>
     <div id="routeContextMenu_inactive"></div>
-
 </body>
 </html>
